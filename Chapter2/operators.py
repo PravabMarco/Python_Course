6fca059278b621b5a8ce0f387c6282b6a064b652
Prabhav Basukala <pravabbasukala@gmail.com>
2026-04-30 23:22:11 +0545

    commit

---
+++ Chapter2/operators.py
@@ -0,0 +1,47 @@
+# Arithmatic Operators
+a = 10
+b = 2
+print(a + b) # Addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a % b) # Modulus
+print(a ** b) # Exponentiation
+
+# Assignment Operators
+x = 5
+x += 3  # x = x + 3
+print(x)
+
+# comparison Operators
+j = 5>2
+print(j) # Greater than
+k = 5<2
+print(k) # Less than
+l = 5==2
+print(l) # Equal to
+m = 5!=2
+print(m) # Not equal to
+
+# Logical Operators
+# Truth Table of 'and' Operator
+
+print("True and False is:", True and False)
+print("True and True is:", True and True)
+print("False and False is:", False and False)
+print("False and True is:", False and True)
+
+# Truth Table of 'or' Operator
+print("True or False is:", True or False) 
+print("True or True is:", True or True)
+print("False or False is:", False or False)
+print("False or True is:", False or True)
+
+# Truth Table of 'not' Operator
+print("not True is:", not True) # Logical NOT
+print("not False is:", not False) # Logical NOT
+print("not (True and False) is:", not (True and False)) # Logical NOT with AND
+print("not (True or False) is:", not (True or False)) # Logical NOT with OR
+
+print (not(True)) # Logical NOT
+print (not(False)) # Logical NOT
---
+++ Chapter2/type.py
@@ -0,0 +1,15 @@
+a = 2
+t = type(a)
+print(t) # class <int>
+
+b = 3.14
+t = type(b)
+print(t) # class <float>
+
+c = "Hello, World!"
+t = type(c)
+print(t) # class <str>
+
+d = True
+t = type(d)
+print(t) # class <bool>
