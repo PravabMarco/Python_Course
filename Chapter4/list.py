03687dd54cca67f7981554e637c0be12f526bc43
Prabhav Basukala <pravabbasukala@gmail.com>
2026-05-14 18:01:32 +0545

    Commit list in python

---
+++ Chapter4/list.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Banana", 1, 10.98, True]
+print(fruits[0])
+print(fruits[3])
+
+fruits[0] = "Mango" # list are mutable and we can change the value of a list
+print(fruits[0:4]) # gives the elements from index 0 to 3 (excluding 4)
---
+++ Chapter4/list_methods.py
@@ -0,0 +1,24 @@
+fruits = ["Apple", "Banana", 1, 10.98, True]
+print(fruits)
+fruits.append("Prabhav") # adds an element to the end of the list
+print(fruits)
+
+friends = ["Prabhav", "Satyarth", "Anshul", "Shivam"]
+#friends.sort() # sorts the list in ascending order
+#friends.reverse() # reverses the order of the list
+#friends.insert(2, "Marco") # inserts an element at a specific index
+#friends.pop(2) # removes the element at index 2 from the list
+#friends.remove("Satyarth") # removes the first occurrence of the element from the list
+# git statusfriends.clear() # removes all the elements from the list
+
+print(friends)
+
+
+# List methods:-
+# list.append()
+# list.insert()
+# list.remove()  
+# list.pop()
+# list.clear()
+# list.sort()
+# list.reverse()
